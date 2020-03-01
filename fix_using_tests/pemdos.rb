b0650ffd60639935669064742ed60eeb5ceda2b9
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-03-01 09:13:40 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,7 +3,10 @@ require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
     binding.pry
-  puts 10.times{prints string[0]}
+    10.times do
+    print "s"
+    end
+  end
   else
   string
   end
